cee094aeb6252c0cff6c84d5f1950250c0b29100
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-12-13 11:52:47 +0100

    data handling for request

---
+++ src/Insertdata.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* Formulardaten entgegennehmen
+*/
+namespace Tualo\Office\ExternalForm;
+use Tualo\Office\ContentManagementSystem\CMSMiddleware;
+
+class Insertdata extends CMSMiddleWare{
+    public static function run(&$request,&$result){
+        @session_start();
+        try{
+            if ( isset($_REQUEST['typ']) ){ // klären DB Zugang
+                if ( $_REQUEST['typ'] == 'rezept'){
+                    // Formular Rezept
+                    // Überprüfung eventuell über $_SERVER['REMOTE_ADDR']
+                    $db->direct("insert into rezepte (name, geburtsdatum, strasse, plz, Ort, telefon, email, rezeptwunsch) 
+                        VALUES 
+                            ('".$_REQUEST['name']."','".$_REQUEST['geburtsdatum']."','".$_REQUEST['strasse']."','".$_REQUEST['plz']."','".$_REQUEST['ort']."','".$_REQUEST['telefon']."','".$_REQUEST['email']."','".$_REQUEST['rezeptwunsch']."','" ,[]);
+
+                } 
+            if ( $_REQUEST['typ'] == 'ueberweisungen'){
+                    // Formular Überweisung
+                    // Überprüfung
+                    $db->direct("insert into ueberweisungen (name, geburtsdatum, strasse, plz, Ort, telefon, email, ueberweisungswunsch) 
+                        VALUES 
+                            ('".$_REQUEST['name']."','".$_REQUEST['geburtsdatum']."','".$_REQUEST['strasse']."','".$_REQUEST['plz']."','".$_REQUEST['ort']."','".$_REQUEST['telefon']."','".$_REQUEST['email']."','".$_REQUEST['ueberweisungswunsch']."','" ,[]);                    
+                } 
+
+            }
+        }catch(\Exception $e){
+    }
+}
