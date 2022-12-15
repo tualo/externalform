f47491d29ad9841a5a1da975b87f99510d1989f2
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-12-15 11:30:31 +0100

    debug info

--- src/Checklogin.php
+++ src/Checklogin.php
@@ -5,9 +5,10 @@ use Tualo\Office\ContentManagementSystem\CMSMiddleware;
 class Checklogin extends CMSMiddleWare{
     public static function run(&$request,&$result){
         @session_start();
+        print_r($_SESSION);
         try{
             if ($_SESSION['wa_session']['login']['loggedIn']===false){
-                header('Location: ./re/login'); /* skljlkjl */
+                // header('Location: ./re/login'); /* skljlkjl */
                 exit();
                 }
         }catch(\Exception $e){
