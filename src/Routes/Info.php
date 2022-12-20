f3bcd93821905ea2fe2c04fad96e0c7f9352b228
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-12-20 14:14:00 +0100

    modify class

--- src/Routes/Info.php
+++ src/Routes/Info.php
@@ -13,6 +13,48 @@ class Route implements IRoute{
         BasicRoute::add('/xyz/info',function($matches){
             App::contenttype('application/json');
             try{
+                $ct=0;
+                
+                $mailText='Folgende neue Rezept- und Überweisungswünsche sind auf der Webseite eingetragen worden:\n';
+                $db  = App::get('session')->getDB();
+                $sql="select count(0) anzahl, 'Überweisungen ' Typ from ueberweisungen where mailsend = 0  and status=0
+                        union
+                    select count(0) anzahl, 'Rezepte ' Typ from rezepte where mailsend = 0  and status=0";
+                $infoData=$db->direct($sql,[]);
+                foreach($infoData as $data){
+                    if($data['anzahl']>0){
+                        $ct++;
+                        $mailText.= $data['typ'].':  '.$data['anzahl'].PHP_EOL;
+                    }
+                }
+                if($ct>0){
+                    // insert in outgoing mails
+                    $insSQL="insert into outgoing_mails (
+                        id,
+                        send_from,
+                        send_from_name,
+                        send_to,
+                        reply_to,
+                        reply_to_name,
+                        subject,
+                        create_date,
+                        body
+                        )VALUES(
+                            uuid(),
+                            'no-reply@tualo.de',
+                            'Webmaster',
+                            'team@hausarztpraxis-roedental.de',
+                            'steffen.borchmann@tualo.de',
+                            'SBO',
+                            'Nachricht von Webseite',
+                            now(),
+                            '".$mailText."')";
+                    // echo PHP_EOL.$insSQL.PHP_EOL;
+                    $db->direct($insSQL,[]);        
+                    $db->direct('update rezepte set mailsend=1',[]);
+                    $db->direct('update ueberweisungen set mailsend=1',[]);
+
+                }
                 App::result('success',true);
             }catch(\Exception $e){
                 App::result('msg',$e->getMessage());
