5c03524d72a1a1446e0d55fb6e6ab188b80b2dd2
Steffen Borchmann <steffen.borchmann@tualo.de>
2022-12-20 10:17:49 +0100

    inserts

--- src/Infomail.php
+++ src/Infomail.php
@@ -20,12 +20,53 @@ class Infomail extends CMSMiddleWare{
             foreach($infoData as $data){
                 if($data['anzahl']>0){
                     $ct++;
-                    $mailText.= $data['typ'].';  '.$data['anzahl'].'\n';
+                    $mailText.= $data['typ'].':  '.$data['anzahl'].PHP_EOL;
                 }
             }
             if($ct>0){
                 // insert in outgoing mails
                 echo $mailText;
+                $insSQL="insert into outgoing_mails (
+                    id,
+                    send_from,
+                    send_from_name,
+                    send_to,
+                    reply_to,
+                    reply_to_name,
+                    subject,
+                    create_date,
+                    body
+                    )VALUES(
+                        uuid(),
+                        'no-replay@hausarztpraxis-roedental.de',
+                        'Webmaster',
+                        'team@hausarztpraxis-roedental.de',
+                        'steffen.borchmann@tualo.de',
+                        'SBO',
+                        'Nachricht von Webseite',
+                        now(),
+                        '".$mailText."')";
+                echo $mailText.PHP_EOL;        
+                // $db->direct('update rezepte set mailsend=1',[]);
+                // $db->direct('update ueberweisungen set mailsend=1',[]);
+                // set mailsend to 1
+/*
+id
+send_from
+send_from_name
+send_to
+reply_to
+reply_to_name
+subject
+create_date
+send_date
+attachment_file
+body
+excpected_response
+tag
+
+*/
+
             }
             echo '<pre>';
             print_r($infoData);
